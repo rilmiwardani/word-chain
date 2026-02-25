63aef7c7f510e14016a4556ecaf2effb456771fa
rilmiwardani <146940091+rilmiwardani@users.noreply.github.com>
2026-02-25 13:43:58 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -667,7 +667,7 @@ export default function App() {
 
   const connectWebSocket = () => {
     const hostname = fallbackToLocalhostRef.current ? "localhost" : window.location.hostname || "localhost";
-    const url = `ws://${hostname}:62024`;
+    const url = `ws://192.168.1.189:62024`;
     wsRef.current = new WebSocket(url);
     wsRef.current.onopen = () => { addLog("System", `Connected to IndoFinity (${hostname})`); fallbackToLocalhostRef.current = false; };
     wsRef.current.onmessage = (event) => {
@@ -2119,3 +2119,4 @@ export default function App() {
     </div>
   );
 }
+
